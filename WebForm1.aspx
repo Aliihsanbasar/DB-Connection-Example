285abccc05fb7fc7134438fd8ca31fffdede7c2c
Ali İhsan Basar <aliihsanbasar@hotmail.com>
2018-08-08 22:43:29 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ProjectCourse.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="ButtonCourse" runat="server" Text="Course" Width="100px" OnClick="ButtonCourse_Click" />
+        <asp:Button ID="ButtonStudent" runat="server" Text="Student" Width="100px" OnClick="ButtonStudent_Click" />
+        <asp:Button ID="ButtonClass" runat="server" Text="Class" Width="100px" OnClick="ButtonClass_Click" />
+        
+        <asp:Button ID="ButtonTakenCourse" runat="server" OnClick="ButtonTakenCourse_Click" Text="Taken Courses" Width="100px" />
+        
+        <br />
+        <br />
+        <br />
+        <br />
+        
+    </div>
+
+    <div>
+        <asp:GridView ID="CourseTable" runat="server" Width="412px" AutoGenerateColumns="False" CellPadding="4" DataSourceID="SqlDataSource1" ForeColor="#333333" GridLines="None" DataKeyNames="Student ID" Height="193px" >
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="Student ID" HeaderText="Student ID" InsertVisible="False" ReadOnly="True" SortExpression="Student ID" />
+                <asp:BoundField DataField="First Name" HeaderText="First Name" SortExpression="First Name" />
+                <asp:BoundField DataField="Surname" HeaderText="Surname" SortExpression="Surname" />
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" HorizontalAlign="Center" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:CourseDBConnectionString4 %>" SelectCommand="SELECT ID as 'Student ID', Name as 'First Name', Surname FROM [Students]"></asp:SqlDataSource>
+    </div>
+    
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+
+namespace ProjectCourse
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection connect;
+        SqlCommand command;
+        SqlDataReader reader;
+        String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=CourseDB;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(connectionString);
+
+            try
+            {
+                connect.Open();
+                Response.Write("Connection Successfull!!!");            //Veritabanı bağlantısını sayfa yüklenirken kontrol
+                connect.Close();                                        //etmek için yazdığım kod bloğudur.
+            }
+            catch (Exception)
+            {
+
+                Response.Write("Check Your Connection!!!");
+            }
+        }
+
+        protected void ButtonCourse_Click(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(connectionString);
+            command = new SqlCommand("Select ID as 'Course ID', Name as 'Course Name' From Courses", connect);
+
+            connect.Open();
+            reader = command.ExecuteReader();
+            CourseTable.Columns.Clear();
+            CourseTable.DataSourceID = null;
+            CourseTable.DataKeyNames = null;
+            CourseTable.AutoGenerateColumns = true;
+            CourseTable.DataSource = reader;
+            CourseTable.DataBind();
+            reader.Close();
+            connect.Close();
+        }
+
+        protected void ButtonStudent_Click(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(connectionString);
+            command = new SqlCommand("Select ID as 'Student ID', Name as 'First Name', Surname From Students", connect);
+
+            connect.Open();
+            reader = command.ExecuteReader();
+            CourseTable.Columns.Clear();
+            CourseTable.DataSourceID = null;
+            CourseTable.DataKeyNames = null;
+            CourseTable.AutoGenerateColumns = true;
+            CourseTable.DataSource = reader;
+            CourseTable.DataBind();
+            reader.Close();
+            connect.Close();
+        }
+
+        protected void ButtonClass_Click(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(connectionString);
+            command = new SqlCommand("Select Class.Name as 'Class Name', Courses.Name as 'Given Course Name', COUNT(Courses.ID) as 'Number Of Students' From TakenCourses INNER JOIN Courses ON TakenCourses.CourseID = Courses.ID INNER JOIN Class ON Courses.ID = Class.CourseID Group By Class.Name, Courses.Name; ", connect);
+
+            connect.Open();
+            reader = command.ExecuteReader();
+            CourseTable.Columns.Clear();
+            CourseTable.DataSourceID = null;
+            CourseTable.DataKeyNames = null;
+            CourseTable.AutoGenerateColumns = true;
+            CourseTable.DataSource = reader;
+            CourseTable.DataBind();
+            reader.Close();
+            connect.Close();
+        }
+
+        protected void ButtonTakenCourse_Click(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(connectionString);
+            command = new SqlCommand("Select Students.Name, Courses.Name From TakenCourses INNER JOIN Students ON TakenCourses.StudentID = Students.ID INNER JOIN Courses ON TakenCourses.CourseID = Courses.ID", connect);
+
+            connect.Open();
+            reader = command.ExecuteReader();
+            CourseTable.Columns.Clear();
+            CourseTable.DataSourceID = null;
+            CourseTable.DataKeyNames = null;
+            CourseTable.AutoGenerateColumns = true;
+            CourseTable.DataSource = reader;
+            CourseTable.DataBind();
+            reader.Close();
+            connect.Close();
+        }
+    }
+}
